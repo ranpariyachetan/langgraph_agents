afcadc57aa084609a4ca2941e42eff80360cf50b
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2025-03-15 10:07:35 +0800

    Added description of prompt chaining.

--- prompt_chaining.py
+++ prompt_chaining.py
@@ -3,6 +3,12 @@ from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from IPython.display import Image, display
 
+# Prompt chaining decomposes a task into a sequence of steps, where each LLM call processes the output of the previous one.
+# You can add programmatic checks (see "gate” in the diagram below) on any intermediate steps to ensure that the process is still on track.
+#
+# When to use this workflow: This workflow is ideal for situations where the task can be easily and cleanly decomposed into fixed subtasks.
+# The main goal is to trade off latency for higher accuracy, by making each LLM call an easier task.
+
 llm = initialize_model()
 
 # Graph State
