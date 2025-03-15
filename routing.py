a99b95dac8812f87709d482908909607b3b5a08a
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2025-03-15 10:54:01 +0800

    Added code to build and display the workflow.

--- routing.py
+++ routing.py
@@ -74,3 +74,32 @@ def route_decision(state: State):
         return "llm_call_2"
     elif state["decision"] == "poem":
         return "llm_call_3"
+
+# Build workflow
+router_builder = StateGraph(State)
+
+router_builder.add_node("llm_call_1", llm_call_1)
+router_builder.add_node("llm_call_2", llm_call_2)
+router_builder.add_node("llm_call_3", llm_call_3)
+router_builder.add_node("llm_call_router", llm_call_router)
+
+router_builder.add_edge(START, "llm_call_router")
+router_builder.add_conditional_edges(
+    "llm_call_router",
+    route_decision,
+    {  # Name returned by route_decision : Name of next node to visit
+        "llm_call_1": "llm_call_1",
+        "llm_call_2": "llm_call_2",
+        "llm_call_3": "llm_call_3",
+    },
+)
+
+router_builder.add_edge("llm_call_1", END)
+router_builder.add_edge("llm_call_2", END)
+router_builder.add_edge("llm_call_3", END)
+
+# Compile Workflow
+router_workflow = router_builder.compile()
+
+# Show the workflow
+display(router_workflow.get_graph().print_ascii())
